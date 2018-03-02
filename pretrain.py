987fe3727738b8c7040c51b9b545275a1efed01c
Regaler <akalsdnr@kaist.ac.kr>
2018-03-02 15:06:35 +0900

    <Pre-training environment for coordination-based approach> Once you got the data, and trimmed the labels, you're ready to pre-train the network. You need to train 4 neural nets since there're 4 robots. example usage: $python3 pretrain.py 0 This will save the weights under '/minuk/save'

---
+++ pretrain.py
@@ -0,0 +1,104 @@
+import argparse
+import skimage as skimage
+from skimage import io
+from skimage import transform, color, exposure
+from skimage.transform import rotate
+from skimage.viewer import ImageViewer
+import sys
+from scipy import misc
+
+import numpy as np
+from collections import deque
+import PIL
+from PIL import Image
+import random
+import pickle
+
+import json
+#from keras import initializers
+#from keras.initializers import normal, identity
+from keras.models import model_from_json
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD , Adam
+import tensorflow as tf
+from keras import losses
+from keras import regularizers
+import keras
+import numpy as np
+
+def buildmodel():
+	model = Sequential()
+	model.add(Dense(64, input_dim = state_size, activation = 'relu'))
+	model.add(Dense(128, activation = 'relu'))
+	model.add(Dense(128, activation = 'relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(label_size, activation = 'softmax', kernel_regularizer = regularizers.l2(0.01))) #activity_regularizer=regularizers.l1(0.01)))
+	model.compile(loss='mse', optimizer = Adam(lr=LEARNING_RATE))
+	return model
+
+if __name__ == '__main__':
+	# parse
+	flag = sys.argv[1]
+	if flag == '0':
+		label_size = 12
+	elif flag == '1':
+		label_size = 14
+	elif flag == '2':
+		label_size = 13
+	elif flag == '3':
+		label_size = 17
+	else:
+		print("Error: The argument must be one of 0, 1, 2, or 3")
+		exit()
+
+	# Iniaizliation
+	LEARNING_RATE = 5*1e-5
+	LABEL = './data/train/label' + str(flag) + '.pkl'
+	COORDINATION = './data/train/COORDINATION' + str(flag) + '.pkl'
+	save_file = './save/weights_FC' + str(flag) + '.h5'
+	BATCH = 64
+	state_size = 47
+	num_of_data = 0
+
+	labels = []
+	states = []
+
+	# Restore data and label
+	with open(LABEL,'rb') as g, open(COORDINATION,'rb') as h:
+		labels = pickle.load(g)[1:]
+		states = pickle.load(h)[1:]
+		num_of_data = len(states)
+
+	# make neural net
+	model = buildmodel()
+	EPOCH = num_of_data*200
+	print("num_of_data: " + str(num_of_data))
+	print("len(states): " + str(len(states)))
+
+	# Run 200 timess
+	for epoch in range(EPOCH):
+		X_temp = np.zeros((BATCH, state_size))
+		Y_temp = np.zeros((BATCH, label_size))
+		cnt_batch = 0
+		print(epoch)
+
+		# Make batch
+		for cnt_batch in range(BATCH):
+			i = int(num_of_data*random.random())
+			X_temp[cnt_batch] = np.array(states[i])
+			Y_temp[cnt_batch] = np.array(labels[i])
+
+		# forward and backward pass
+		model.fit(X_temp, Y_temp, epochs=1)
+
+		# Print loss at the end
+		if epoch == EPOCH - 1:
+			Y_pred = model.predict(X_temp)
+			print("true: " + str(Y_temp))
+			print("pred: " + str(Y_pred))
+
+		# Save weights
+		if epoch % 1 == 0 and epoch > 1:
+			model.save_weights(save_file, overwrite=True)
---
+++ pretrain_test_v3.py
@@ -0,0 +1,147 @@
+import argparse
+import skimage as skimage
+from skimage import io
+from skimage import transform, color, exposure
+from skimage.transform import rotate
+from skimage.viewer import ImageViewer
+import sys
+from scipy import misc
+
+import numpy as np
+from collections import deque
+import PIL
+from PIL import Image
+import random
+import pickle
+
+import json
+#from keras import initializers
+#from keras.initializers import normal, identity
+from keras.models import model_from_json
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD , Adam
+from keras import regularizers
+#import keras.backend
+import tensorflow as tf
+from keras import losses
+import keras
+import numpy as np
+import sys
+
+# Neural network
+def buildmodel():
+	model = Sequential()
+	model.add(Dense(64, input_dim = state_size, activation = 'relu'))
+	model.add(Dense(128, activation = 'relu'))
+	model.add(Dense(128, activation = 'relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(label_size, activation = 'softmax', kernel_regularizer = regularizers.l2(0.01))) #activity_regularizer=regularizers.l1(0.01)))
+	model.compile(loss='mse', optimizer = Adam(lr=LEARNING_RATE))
+	return model
+
+if __name__ == 	"__main__":
+	flag = sys.argv[1]
+	num_flag = sys.argv[2]
+	label_size = 0
+	
+	#Convert image into Black and white
+	if flag == "--train":
+		STATE = './data/COORDINATION' + num_flag + '.pkl'
+		LABEL0 = './data/label' + num_flag + '_trimed.pkl'
+	elif flag == "--test":
+		STATE = './data/COORDINATION' + num_flag + '_test.pkl'
+		LABEL0 = './data/label' + num_flag + '_test_trimed.pkl'
+	else:
+		print("Error: No such flag. You can only input --train or --test")
+
+	num = int(num_flag)
+	if num == 0:
+		print("0000000000000000000")
+		label_size = 12
+	elif num == 1:
+		print("1111111111111111111111111")
+		label_size = 14
+	elif num == 2:
+		print("22222222222222")
+		label_size = 13
+	elif num == 3:
+		print("33333333333333")
+		label_size = 17
+	else:
+		print("Error: No such robot. You should give 0, 1, 2, or 3")
+
+	LEARNING_RATE = 1e-5
+	state_size = 47
+	batch_size = 32
+	num_of_data = 0
+	states = []
+	labels = []
+	loss = 0
+	prediction_success_ratio = 0
+
+	# <1> make neural net
+	model = buildmodel()
+	model.load_weights("./save/weights_FC" + num_flag + ".h5")
+
+	# <2> Restore data and label
+	with open(STATE,'rb') as f, open(LABEL0,'rb') as g:
+		states = pickle.load(f)
+		labels = pickle.load(g)
+	states = states[2:]
+	labels = labels[2:]
+	num_of_data = len(labels)
+
+	# <3> Run 5 times for no reason
+	for epoch in range(100):
+		cnt = 0
+		cnt_total = 0
+		cnt_batch = 0
+		X = np.zeros((batch_size,state_size))
+		Y = np.zeros((batch_size,label_size))
+		print(epoch)
+
+		# Make batch of size 32
+		for cnt_batch in range(batch_size):
+			i = int(num_of_data*random.random())
+			X[cnt_batch] = states[i]
+			Y[cnt_batch] = labels[i]
+
+		# Predict and calculate loss
+		Y_predict = model.predict(X)
+		loss_temp = ((Y_predict - Y) ** 2).mean()
+		loss += loss_temp
+		print("true: " + str(Y))
+		print("pred: " + str(Y_predict))
+		print("loss :" + str(loss_temp))
+
+		for i in range(32):
+			if np.argmax(Y[i])==np.argmax(Y_predict[i]):
+				cnt+=1
+			cnt_total+=1
+
+		# Write the result to file
+		with open("./data/test_result_sub.txt",'a') as ff:
+			ff.write("**************************\n")
+			for j in range(32):
+				ff.write("action number: " + str(np.argmax(Y[j])) + ", predicted action number: " + str(np.argmax(Y_predict[j])) +"\n")
+			ff.write("Prediction success ratio: " + str(cnt/cnt_total) + "\n")
+		prediction_success_ratio += cnt/cnt_total
+
+
+		with open("./data/test_result.txt",'a') as f:
+			f.write("**************************\n")
+			f.write(str(epoch) + " -th epoch\n")
+			f.write("**************************\n")
+			f.write("true\n")
+			f.write(str(Y))
+			f.write("\npred\n")
+			f.write(str(Y_predict))
+			f.write("\n loss \n")
+			f.write(str(loss_temp))
+
+	loss /= 100
+	print("average loss : " + str(loss))
+	prediction_success_ratio /= 100
+	print("average prediction_success_ratio : " + str(prediction_success_ratio))
